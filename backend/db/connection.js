18423e8e1fc7454821dae5a6a49b486c8a483c0e
mrkabby <antwikenneth14.com>
2024-03-20 15:00:08 +0000

    add node cmd

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,21 +1,13 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { MongoClient, } from "mongodb";
 
-const uri = process.env.ATLAS_URI || "";
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-}
-);
-
+// const uri = process.env.ATLAS_URI || "";
+const client = new MongoClient(process.env.ATLAS_URI);
 const DATABASE_NAME = "portfolio_db"
 
 try {
     await client.connect();
 
-    await client.db('admin').command({ping: 186});
+    await client.db('admin').command({ ping: 1 });
     console.log(" youv've successfully conneccted to Mongodb")
 } catch (error) {
     console.log(error);
